f0e7b15a89f30cefa04d90f1f77bc0c36d12f18e
TimoRenken <timorenken@outlook.de>
2024-03-06 21:46:27 +0100

    first style of small cards

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,14 +1,17 @@
 function generatePokemonList(pokemon){
+    let name = pokemon['name'];
+    let formattedName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase(); // returns first letter uppercased
+
     return /*html*/ `
         <div id="small_card">
             <div>
-                <h1>${pokemon['name']}</h1>
+                <h1>${formattedName}</h1>
                 <div>${pokemon['types']['0']['type']['name']}</div>
                 <div>${pokemon['types'][1] ? `<div>${pokemon['types']['1']['type']['name']}</div>` : ''}</div>
             </div>
             <div>
-                <div>${pokemon['id']}</div>
-                <img src="${pokemon['sprites']['other']["official-artwork"]['front_default']}" alt="picture of Pokemon">
+                <div class="card_id">#${pokemon['id']}</div>
+                <img class="small_card_img" src="${pokemon['sprites']['other']["official-artwork"]['front_default']}" alt="picture of Pokemon">
             </div>
         </div>`;
 }
