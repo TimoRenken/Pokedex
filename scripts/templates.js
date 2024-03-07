7f0dd8debf4860cb6c7a538d879efaaba885396c
TimoRenken <timorenken@outlook.de>
2024-03-07 17:11:31 +0100

    added background-colors

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,20 +1,28 @@
-function generatePokemonList(pokemon){
+
+// Displays small Pokemon Cards
+function generatePokemonList(pokemon) {
     let name = pokemon['name'];
-    let formattedName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase(); // returns first letter uppercased
+    let formattedName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase(); // returns first letter uppercased, rest lowercased.
 
     return /*html*/ `
         <div id="small_card">
-            <div>
-                <h1>${formattedName}</h1>
-                <div>${pokemon['types']['0']['type']['name']}</div>
-                <div>${pokemon['types'][1] ? `<div>${pokemon['types']['1']['type']['name']}</div>` : ''}</div>
+            <div class="small_card_body">
+                 <div class="d_flex_sb_c small_card_header">
+                    <h1 class="color_white">${formattedName}</h1>
+                    <div class="card_id">#${pokemon['id']}</div>
+                </div>
+                <div class="d_flex_sb_c">
+                   <div class="types_small">
+                     <div>${pokemon['types']['0']['type']['name']}</div>
+                     <div>${pokemon['types'][1] ? `<div>${pokemon['types']['1']['type']['name']}</div>` : ''}</div>
+                   </div>
+               <div>
+                 <img class="small_card_img" src="${pokemon['sprites']['other']["official-artwork"]['front_default']}" alt="picture of Pokemon">
+               </div>
+             </div>
             </div>
-            <div>
-                <div class="card_id">#${pokemon['id']}</div>
-                <img class="small_card_img" src="${pokemon['sprites']['other']["official-artwork"]['front_default']}" alt="picture of Pokemon">
-            </div>
+           
         </div>`;
 }
 
 /* <div class="info_container"></div> // wird für die große Karte gebraucht. */
-    
